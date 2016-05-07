478a8c6007152764d6da58a26395e4ee3538e4b3
jumblesale <rob@robsollars.co.uk>
2016-05-07 23:01:29 +0100

    added a (dubious) method for checking if a user exists

--- lib/users.py
+++ lib/users.py
@@ -1,7 +1,12 @@
+import os
+
 
 def user_exists(user):
-    return True
+    return os.path.exists("%s/%s" % ("/home", user))
 
 
 def user_page_exists(user, page):
     return False
+
+if __name__ == "__main__":
+    print user_exists("jumblesale")
